0be3622317f958a1f75d080b831ea37a947b2fc3
Daisuke Miyakawa <d.miyakawa+mac@gmail.com>
2015-01-09 19:45:08 +0900

    Make check_purchases.rb work with current HTML

--- check_purchases.rb
+++ check_purchases.rb
@@ -13,6 +13,7 @@
 # 3. 取得した情報から、csv 形式で購入物一覧表を作成する。
 #   $ ruby make-index.rb > 1.csv
 
+require 'csv'
 require 'rubygems'
 require 'optparse'
 require 'ostruct'
@@ -23,7 +24,29 @@ require 'selenium-webdriver'
 SCREENSHOTS_DIR = './screenshots'
 
 module Amazon
+  def self.instantiate(email, password=nil, logger=nil)
+    if password.nil?
+      print "password: "
+      password = STDIN.noecho(&:gets)
+      puts
+    end
+    driver = Amazon::Driver.new(logger)
+    driver.login(email, password)
+  end
+
   class Driver
+    attr_reader :wd
+
+    def initialize(logger=nil)
+      @wd = Selenium::WebDriver.for(:firefox)
+      @wd.manage.timeouts.implicit_wait = 10 # sec
+      @logger = logger || Logger.new('/dev/null')
+    end
+
+    def close()
+      @wd.quit
+    end
+
     # 新しいタブで 指定された URL を開き、制御をそのタブに移す。
     def open_new_window(wd, url)
       a = wd.execute_script("var d=document,a=d.createElement('a');a.target='_blank';a.href=arguments[0];a.innerHTML='.';d.body.appendChild(a);return a", url)
@@ -49,48 +72,87 @@ module Amazon
       end
     end
 
-    def save_order_history(wd, auth)
-      @page_seq = 0
-      @order_seq = 0
+    def login(email, password)
+      @wd.get 'https://www.amazon.co.jp/gp/css/order-history'
+      wait = Selenium::WebDriver::Wait.new(:timeout => 3)
+      wait.until { @wd.find_element(:id, 'signInSubmit-input').displayed? }
+      @wd.find_element(:id, 'ap_email').click
+      @wd.find_element(:id, 'ap_email').clear
+      @wd.find_element(:id, 'ap_email').send_keys email
+      @logger.info("Hoge")
+      @wd.find_element(:id, 'ap_password').click
+      @wd.find_element(:id, 'ap_password').clear
+      @wd.find_element(:id, 'ap_password').send_keys password
+      # input = @wd.find_element(:id, 'signInSubmit-input')
+      # input.click
+      self
+    end
 
-      # 購入履歴ページへ
-      wd.get 'https://www.amazon.co.jp/gp/css/order-history'
+    def contain(class_name)
+      "contains(concat(' ',normalize-space(@class),' '), ' #{class_name} ')"
+    end
 
-      # ログイン処理
-      wd.find_element(:id, 'ap_email').click
-      wd.find_element(:id, 'ap_email').clear
-      wd.find_element(:id, 'ap_email').send_keys auth[:email]
-
-      wd.find_element(:id, 'ap_password').click
-      wd.find_element(:id, 'ap_password').clear
-      wd.find_element(:id, 'ap_password').send_keys auth[:password]
-
-      wd.find_element(:id, 'signInSubmit-input').click
-
-      unless wd.find_element(:xpath, "//form[@id='order-dropdown-form']/select//option[4]").selected?
-        wd.find_element(:xpath, "//form[@id='order-dropdown-form']/select//option[4]").click  # 今年の注文
+    def run(csv=nil)
+      run_single_page(csv)
+      while go_next()
+        run_single_page(csv)
       end
-      wd.find_element(:css, "#order-dropdown-form > span.in-amzn-btn.btn-prim-med > span > input[type=\"submit\"]").click
+    end
 
-      # [次] ページをめくっていく
-      loop do
-        wd.find_element(:link_text, '利用規約')
-        @page_seq += 1
-        wd.save_screenshot("#{SCREENSHOTS_DIR}/page_#{format('%03d', @page_seq)}.png")
-        open("#{SCREENSHOTS_DIR}/page_#{format('%03d', @page_seq)}.html", 'w') {|f|
-          f.write wd.page_source
-        }
+    def run_single_page(csv=nil)
+      logger = @logger
+      orders = @wd.find_elements(:xpath, "//div[#{contain('order')}]")
+      orders.each do |order|
+        rows = order.find_elements(:xpath, "div[not(#{contain('order-attributes')})]")
+        orderinfo_row = rows[0]
+        order_inner = orderinfo_row.find_element(:xpath,
+                                                 ("div/div/div"\
+                                                  + "[#{contain('a-fixed-right-grid-inner')}]"))
+        date = order_inner.find_element(:xpath, "div[1]/div/div[1]/div[2]/span").text
+        price = order_inner.find_element(:xpath, "div[1]/div/div[2]/div[2]/span").text
+        order_id = order_inner.find_element(:xpath, "div[2]/div[1]/span[2]").text
+        rows[1..-1].each_with_index do |row, i|
+          # Item Name, Author, Thumbnail
+          item_info_div = row.find_element(:xpath,
+                                           ("div/div/div/div/div/div["\
+                                            + "#{contain('a-fixed-left-grid')}]"))
+          item_a = item_info_div.find_element(:xpath,
+                                              ("div/div[#{contain('a-col-right')}]/"\
+                                               + "div[1]/a"))
+          item_name = item_a.text
+          item_url = item_a.attribute('href')
 
-        # ページ中の個々の注文を閲覧する。
-        save_order(wd)
-
-        elems = wd.find_elements(:link_text, '次へ »')
-        break if elems.size == 0
-        elems[0].click
+          puts "#{item_name} (#{item_url})"
+          puts "#{date}, #{price}, #{order_id}"
+          unless csv.nil?
+            if i == 0
+              entry = [item_name, item_url, date, price, order_id]
+            else
+              entry = [item_name, item_url, '', '', '']
+            end
+            csv << entry
+          end
+        end
       end
+      self
+    end
 
-      # サインアウト
-      wd.get 'http://www.amazon.co.jp/gp/flex/sign-out.html/ref=gno_signout'
+    def go_next()
+      # "次へ" (Next) link
+      last_item = @wd.find_element(:xpath, ("//ul[@class='a-pagination']"\
+                                            + "/li[#{contain('a-last')}]"))
+      a_lst = last_item.find_elements(:xpath, 'a')
+      if a_lst.empty?
+        return false
+      else
+        @wd.get a_lst[0].attribute('href')
+        return true
+      end
+    end
+
+    def logout()
+      @wd.get 'http://www.amazon.co.jp/gp/flex/sign-out.html/ref=gno_signout'
+      self
     end
   end
 end
@@ -104,7 +166,7 @@ module Main
     logger.level = Logger::INFO
     parser = OptionParser.new
     begin
-      parser.banner = "Usage: #{File.basename($0)} account"
+      parser.banner = "Usage: #{File.basename($0)} email"
       parser.on('-h', '--help', 'print this message and quit.') do
         puts parser.help
         exit 0
@@ -117,32 +179,26 @@ module Main
     end
     return opts, logger
   end
-  def run()
+  def run(argv)
     opts, logger = Main.parse_options()
     include Amazon
-    if ARGV.size != 1
-      puts "usage: ruby #{$PROGRAM_NAME} account"
-      exit 1
+    if argv.size != 1
+      puts "usage: ruby #{$PROGRAM_NAME} email"
+      return false
     end
-  
-    print "password: "
-    account = ARGV[0]
-    password = STDIN.noecho(&:gets)
-    puts
-  
-    wd = nil
+    email = argv[0]
+    ad = nil
     begin
-      ad = Amazon::Driver.new
-      wd = Selenium::WebDriver.for :firefox
-      wd.manage.timeouts.implicit_wait = 20 # sec
-      ad.save_order_history(wd, email: account, password: password)
+      ad = Amazon.instantiate(email, nil, logger)
+      ad.run_single_page()
     ensure
-      wd.quit if wd
+      ad.close unless ad.nil?
     end
+    return true
   end
 end
 
 if File.basename($0) == File.basename(__FILE__)
-  Main.run()
+  exit Main.run(ARGV)
 end
 
